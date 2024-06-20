d77ff3669d0ab54aa106c0afaf697ffa7d9897ab
enirdev <deusenirfranca.ti@gmail.com>
2024-06-20 18:13:46 -0300

    concantenaçao

--- Tipos-variaveis/src/variaveis.java
+++ Tipos-variaveis/src/variaveis.java
@@ -8,29 +8,9 @@ public class variaveis {
      */
     public static void main (String []args) throws exception {
 
-            short numeroCurto = 1;
-            int numeroNormal = numeroCurto;
-            short numeroCurto2= (short) numeroNormal;
-            String nome = "Deusenir";
-            int idade = 22;
-            double peso = 68.5;
-            char sexo = 'M';
-            boolean doadorOrgao = false;
-            Date dataNascimento = new Date()
-
-
-            //Operador aritmético://
-
-
-            double  soma = 10.5 + 15.7;
-            int subtração = 113 -25;
-            int multiplicaçao = 20*7;
-            int divisao = 15/3;
-            int modulo = 18 % 3;
-            double resultado = (10*7) + (20/4);
-
-
             
+
+
 
 
 
